e8a8b9390433fb76cac38bfd0bfed6e135190c0a
Simon Arlott <sa.me.uk>
2016-03-20 00:14:50 +0000

    output SSHFP separately

--- sshfp.py
+++ sshfp.py
@@ -32,13 +32,20 @@ def sshfp(filename):
 		key_data = f.readline().split(" ")[1]
 		key_data = base64.b64decode(key_data)
 
-	visual_key[1] += "  MD5:"
-	visual_key[2] += "    " + __with_colons(hashlib.md5(key_data).hexdigest())
-	visual_key[4] += "  SHA1:"
-	visual_key[5] += "    " + __with_colons(hashlib.sha1(key_data).hexdigest())
-	visual_key[7] += "  SHA256:"
-	visual_key[8] += "    " + base64.b64encode(hashlib.sha256(key_data).digest()).decode("ascii").strip("=")
-	visual_key[9] += "    " + hashlib.sha256(key_data).hexdigest().upper()
+	sshfp_type = "?"
+	for (key, value) in { "[RSA ": 1, "[DSA ": 2, "[ECDSA ": 3, "[ED25519 ": 4, }.items():
+		if key in visual_key[0]:
+			sshfp_type = value
+	sshfp_hash = hashlib.sha256(key_data).hexdigest().upper()
+
+	visual_key[0] += "  MD5:"
+	visual_key[1] += "    " + __with_colons(hashlib.md5(key_data).hexdigest())
+	visual_key[3] += "  SHA1:"
+	visual_key[4] += "    " + __with_colons(hashlib.sha1(key_data).hexdigest())
+	visual_key[6] += "  SHA256:"
+	visual_key[7] += "    " + base64.b64encode(hashlib.sha256(key_data).digest()).decode("ascii").strip("=")
+	visual_key[9] += "  SSHFP:"
+	visual_key[10] += "    " + str(sshfp_type) + " 2 " + sshfp_hash[0:56] + " " + sshfp_hash[56:]
 	print("\n".join(visual_key))
 
 if __name__ == "__main__":
